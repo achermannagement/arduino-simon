bcc5ca744d64aded158b2fa842319f749ab4a5e5
Joshua Achermann <joshua.achermann@gmail.com>
2018-09-01 12:01:50 +1000

    Refactored pattern to be a finite state machine

--- simon_says.ino
+++ simon_says.ino
@@ -9,17 +9,33 @@ typedef unsigned long time_t;
 #define SIMON_PATTERN_LENGTH 12
 #define LIGHT_BLINK_MILLI 400
 #define PATTERN_SPACING_MILLI 2000
+enum SIMON_STATE {
+  FLASH,
+  BLINK_WAIT,
+  BLANK,
+  PATTERN_SPACING
+};
 typedef struct {
   int8_t pattern[SIMON_PATTERN_LENGTH]; // correct pattern
   int8_t attempt[SIMON_PATTERN_LENGTH]; // stores user attempt
   int8_t curr_index_pattern;
-  bool pattern_next;
-  bool blank_all;
+  SIMON_STATE state;
   time_t last_update;
   time_t now;
 } simon_round_data;
 
 simon_round_data data;
+
+void blank_all() {
+  for(int i = 0; i < SIMON_PATTERN_LENGTH; i++){
+    digitalWrite(i, LOW); // not the most efficient way, but easy
+  }
+}
+
+void setup_state(SIMON_STATE s) {
+  data.last_update = millis();
+  data.state = s;
+}
 
 void setup() {
   randomSeed(analogRead(0)); // assumes pin0 is unconnected for entropy sake
@@ -29,7 +45,7 @@ void setup() {
     data.pattern[i] = random(SIMON_LED0, SIMON_LED3 + 1); // assumes SIMON_LEDs are sequential
   }
   data.curr_index_pattern = 0;
-  data.blank_all = true;
+  data.state = FLASH;
   data.last_update = millis();
 
   pinMode(SIMON_LED0, OUTPUT);
@@ -40,24 +56,49 @@ void setup() {
 
 void loop() {
   data.now = millis();
-  if(data.blank_all) { // TODO: this could be a finite state machine
-    if(data.now > data.last_update + LIGHT_BLINK_MILLI){
-      data.pattern_next = true;
-      data.blank_all = false;
+
+  switch (data.state) {
+    case FLASH:
+    {
+      // blink current light
+      digitalWrite(data.pattern[data.curr_index_pattern], HIGH);
+      data.curr_index_pattern = (data.curr_index_pattern + 1) % SIMON_PATTERN_LENGTH;
+      setup_state(BLINK_WAIT);
     }
-    for(int i = 0; i < SIMON_PATTERN_LENGTH; i++){
-      digitalWrite(i, LOW); // not the most efficient way, but easy
+    break;
+
+    case BLINK_WAIT:
+    {
+      if(data.now > data.last_update + LIGHT_BLINK_MILLI){
+        setup_state(BLANK);
+      }
     }
-  } else if(data.pattern_next) {
-    data.pattern_next = false;
-    data.last_update = millis();
-    digitalWrite(data.pattern[data.curr_index_pattern], HIGH);
-    data.curr_index_pattern = (data.curr_index_pattern + 1) % SIMON_PATTERN_LENGTH;
-  } else {
-    // todo: set spacing between patterns
-    if(data.now > data.last_update + LIGHT_BLINK_MILLI){
-      data.blank_all = true;
-      data.last_update = millis();
+    break;
+    
+    case BLANK:
+    {
+      blank_all();
+      if(data.now > data.last_update + LIGHT_BLINK_MILLI){
+        if(data.curr_index_pattern == 0) {
+          setup_state(PATTERN_SPACING);
+        } else {
+          setup_state(FLASH);
+        }
+      }
+    }
+    break;
+
+    case PATTERN_SPACING:
+    {
+      if(data.now > data.last_update + PATTERN_SPACING_MILLI){
+        setup_state(FLASH);
+      }
+    }
+    break;
+
+    default:
+    {
+      // TODO: error recovery?
     }
   }
 }
