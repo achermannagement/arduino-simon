615cf3c2e01f1d73faf40c98ef9bc5500a3f7050
Joshua <joshua.achermann@gmail.com>
2018-11-04 12:36:24 +1100

    Added LGPL license

--- simon_says.ino
+++ simon_says.ino
@@ -1,4 +1,23 @@
-//TODO: led mapping could be abstracted to be less trash
+/**
+ * Simon implemented on Arduino
+    Copyright (C) 2018  Joshua Achermann
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    email: joshua.achermann@gmail.com
+ */
+
 typedef unsigned long time_t;
 
 const int8_t SIMON_LED0 = 2;
